0f4499115eb8537af205f3cf2fee973013973241
ReR-7 <fernando_1320114003@uptecamac.edu.mx>
2023-07-11 23:49:52 -0600

    Comenarios

--- Accel/MainPage.xaml.cs
+++ Accel/MainPage.xaml.cs
@@ -16,35 +16,49 @@ namespace Accel
             InitializeComponent();
         }
 
+        //Verifica si el acelerómetro ya está siendo monitoreado y, si no lo está, lo inicia con la velocidad predeterminada
         protected override void OnAppearing()
         {
             base.OnAppearing();
 
+            // Verificar si el acelerómetro ya está siendo monitoreado
             if (Accelerometer.IsMonitoring)
                 return;
 
+            // Iniciar el monitoreo del acelerómetro con la velocidad predeterminada
             Accelerometer.Start(SensorSpeed.Default);
+
+            // Suscribirse al evento ReadingChanged para recibir notificaciones de cambios en los valores del acelerómetro
             Accelerometer.ReadingChanged += Acelerometro_ReadingChanged;
         }
 
+        //sDetiene el monitoreo del acelerómetro llamando a Accelerometer.Stop() y se desuscribe del evento ReadingChanged
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
 
+            // Detener el monitoreo del acelerómetro
             Accelerometer.Stop();
+
+            // Desuscribirse del evento ReadingChanged para dejar de recibir notificaciones de cambios en los valores del acelerómetro
             Accelerometer.ReadingChanged -= Acelerometro_ReadingChanged;
         }
 
+        //se ejecuta cada vez que se produce un cambio en los valores del acelerómetro
         void Acelerometro_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
         {
+            // Obtener los datos del acelerómetro del evento
             var data = e.Reading;
 
+            // Extraer los valores de aceleración en los ejes X, Y y Z
             var x = data.Acceleration.X;
             var y = data.Acceleration.Y;
             var z = data.Acceleration.Z;
 
+            // Actualizar la interfaz de usuario en el subproceso principal
             Device.BeginInvokeOnMainThread(() =>
             {
+                // Asignar la cadena formateada al Label para mostrar los valores del acelerómetro
                 accelerometerLabel.Text = $"X: {x:F2}\nY: {y:F2}\nZ: {z:F2}";
             });
         }
